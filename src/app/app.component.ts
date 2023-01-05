74c66b768a70be8e6f9c397b392591ce36744de1
RodResq <rodrigoresque@yahoo.com.br>
2023-01-05 10:49:56 -0300

    updt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators/map'
 
+import { Post } from './post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,8 +18,8 @@ export class AppComponent implements OnInit {
     this.fetchPost();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
-    this.http.post(
+  onCreatePost(postData: Post) {
+    this.http.post<{ name: string}>(
       'https://ng-complete-guide-7fa1f-default-rtdb.firebaseio.com/posts.json', 
       postData
     ).subscribe(response => {
@@ -35,9 +37,9 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPost() {
-    this.http.get('https://ng-complete-guide-7fa1f-default-rtdb.firebaseio.com/posts.json')
+    this.http.get<{[key: string]: Post}>('https://ng-complete-guide-7fa1f-default-rtdb.firebaseio.com/posts.json')
       .pipe(map(responseData => {
-        const postArray = [];
+        const postArray: Post[] = [];
         for (const  key in responseData) {
           if (responseData.hasOwnProperty(key)) {
             postArray.push({ ...responseData[key], id: key })
@@ -45,8 +47,8 @@ export class AppComponent implements OnInit {
         }
         return postArray;
       }))
-      .subscribe(resp => {
-        console.log(resp);
+      .subscribe(post => {
+        this.loadedPosts = post
       });
   }
 }
