11e5b6a0065a8601c78fa2177486190fc8aa43dd
RodResq <rodrigoresque@yahoo.com.br>
2023-01-06 10:49:36 -0300

    updt

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -35,4 +35,8 @@ export class PostsService {
         }));
     }
 
+    deletePosts() {
+        return this.http.delete('https://ng-complete-guide-7fa1f-default-rtdb.firebaseio.com/posts.json');
+    }
+
 }
